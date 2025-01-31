d8828c44e1e97ebd213ac12119b596a46e57279f
Navin Kumar <hacktheplanet0062@gmail.com>
2025-02-01 00:34:43 +0530

    added

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  transpilePackages: ['react-reveal'],
   webpack: (config) => {
     config.module.rules.push({
       test: /\.(mp3|wav)$/i,
@@ -9,6 +10,13 @@ const nextConfig = {
         loader: "url-loader",
       },
     });
+
+    // Resolve react to a single version
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'react': require.resolve('react'),
+      'react-dom': require.resolve('react-dom')
+    };
 
     return config;
   },
